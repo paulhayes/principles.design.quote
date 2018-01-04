28babc4f366fed82750aeeae967a64263ef302e6
Paul Hayes <paul.hayes@entropedia.co.uk>
2018-01-04 18:46:19 +0000

    Made service.js respond in Alexa json format. see https://developer.amazon.com/docs/custom-skills/request-and-response-json-reference.html

--- service.js
+++ service.js
@@ -4,11 +4,22 @@ const https = require("https");
 const http = require("http");
 const principleDesign = require("./principle-design.js");
 
+const alexaResponse = {
+	"version": "string",
+	"response": {
+	    "outputSpeech": {
+	      "type": "PlainText",
+	      "text": "Plain text string to speak"
+    	}
+    }
+};
+
 const server = http.createServer(function(req,res){
 	res.writeHead(200,{ 'Content-Type': 'application/json' });
 
 	principleDesign.getQuote().then(function(quote){
-		res.write(quote);
+		alexaResponse.response.text = quote;
+		res.write(JSON.stringify(alexaResponse));
 		res.end();
 	});
 
